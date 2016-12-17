bf70391e8bd669d9223110ed096df68ea54573e3
aranga-au <aranga.nanayakkara@gmail.com>
2016-12-18 01:34:44 +1100

    update

--- api/server.js
+++ api/server.js
@@ -8,26 +8,26 @@ app.use(bodyParser.json());
 //options 
 var permissionDef={
     'USER_ROLE':1,
-    'ADMIN_ROLE':32,
+    'ADMIN_ROLE':2,
     'SITE_OWNER_ROLE':4,
     'CATEGORY_WRITE':8,
-    'CATEGORY_DELETE':16
+    'CATEGORY_DELETE':16,
+    'CATEGORY_DELETE':32
 
 };
 var acl ={
-    '/category':{
-        'GET':['USER_ROLE','ADMIN_ROLE'],
+    '/category/*':{
+        'GET':['ADMIN_ROLE'],
         'POST':['ADMIN_ROLE'],
         'PUT':[],
         'DELETE':[]
      },
-    '/category/{id}':{
-        'GET':['USER_ROLE','ADMIN_ROLE'],
-        'POST':[],
-        'PUT':['ADMIN_ROLE','CATEGORY_WRITE'],
-        'DELETE':['ADMIN']
+      '/category/group/*':{
+        'GET':['SITE_OWNER_ROLE','ADMIN_ROLE'],
+        'POST':['ADMIN_ROLE'],
+        'PUT':[],
+        'DELETE':[]
      },
-    
 };
 var options={
     expiresIn:320000,
@@ -36,20 +36,30 @@ var options={
 };
 var aclManger = require('jwt-acl-manager')('password',options,acl,permissionDef);
 
-app.use(function(req,res,next){
- console.log(req.originalUrl);
- req.resource =   req.originalUrl;
- next();
-});
 
-app.use(aclManger.accessController);
+app.use(aclManger.accessController());
 
 app.get('/', function (req, resp) {
-    resp.send({ id: 1, message: "hello" });
+    resp.send({ id: 1, message: "hello" },200);
 });
 app.get('/category', function (req, resp) {
+    console.log(req);
+    resp.send({ id: new Date().getTime(), message: "category access no id" },200);
+});
+app.get('/category/:id', function (req, resp) {
+    console.log(req.route.path);
+    //console.log(req);
     resp.send({ id: new Date().getTime(), message: "category access" },200);
 });
+app.get('/category/group/my', function (req, resp) {
+ 
+    //console.log(req);
+    resp.send({ id: new Date().getTime(), message: "category access group my" },200);
+});
+app.get('/list', function (req, resp) {
+    resp.send({ id: new Date().getTime(), message: "un protected route" },200);
+});
+
 app.post('/login', function (req, resp) {
     var user = req.body;
     console.log(user);
@@ -68,7 +78,7 @@ app.post('/login', function (req, resp) {
         loggedInAs: u.permission
     }
     aclManger.generateToken(payLoad).then(function (token) {
-        resp.send({ userId: u.userId, token: token });
+        resp.send({ userId: u.userId, auth_token: token });
     }).catch(function (e) {
         resp.status(500).send(e);
 
@@ -79,5 +89,5 @@ var server = app.listen(4000, function (err) {
         console.log(err);
         return;
     }
-    console.log('server is running on :', server.address().port);
+    console.log('server is listening on :', server.address().port);
 });
